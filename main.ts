654c5e2df5f493b296874115c9843470ee24e6f4
zhandroids <862340288@qq.com>
2021-09-12 21:57:24 +0800

    增加初始化

--- main.ts
+++ main.ts
@@ -11,6 +11,21 @@ enum vocabularyList{
 
 namespace mASR{
 
+    /**
+     * Connects to the Serial voice Recognition.
+     * @param asrRX voice Recognition device receiver pin (RX), eg: DigitalPin.P0
+     * @param asrTX voice Recognition device transmitter pin (TX), eg: DigitalPin.P1
+     */
+    //% blockId="makerbit_asr_connect" block="connect ASR device with ASR RX attached to %asrRX | and ASR TX to %asrTX"
+    //% asrRX.fieldEditor="gridpicker" asrRX.fieldOptions.columns=3
+    //% asrRX.fieldOptions.tooltips="false"
+    //% asrTX.fieldEditor="gridpicker" asrTX.fieldOptions.columns=3
+    //% asrTX.fieldOptions.tooltips="false"
+    //% weight=50 
+    export function connectSerialASR(asrRX: DigitalPin, asrTX: DigitalPin): void {
+        serial.redirect(asrRX as number, asrTX as number, BaudRate.BaudRate9600);
+    }
+
     let asrEventId = 3500
     let lastvoc = vocabularyList.VOICE_YES
     //% block="ASR sensor IIC port hear %vocabulary"
@@ -43,4 +58,27 @@ namespace mASR{
         })
 
     }
+
+    //% block="ASR sensor IIC port hear %vocabulary"
+    //% result.fieldEditor="gridpicker" vocabulary.fieldOptions.columns=3
+    //% blockId = ASR
+    export function delimiters(vocabulary: vocabularyList): boolean {
+        const readData = serial.readBuffer(1).toArray(NumberFormat.UInt8BE);
+
+    
+        if (225 == readData[0]) {
+            return readData[0] == vocabularyList.VOICE_225;
+        } else if (226 == readData[0]) {
+            return readData[0] == vocabularyList.VOICE_226;
+        } else if (227 == readData[0]) {
+            return readData[0] == vocabularyList.VOICE_227;
+        } else if (228 == readData[0]) {
+            return readData[0] == vocabularyList.VOICE_228;
+        } else if (229 == readData[0]) {
+            return readData[0] == vocabularyList.VOICE_229;
+        }
+        return false
+    }
+
+    
 }
