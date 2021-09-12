67782efea58ac2afe80d6c951331324243f52e0a
zhandroids <862340288@qq.com>
2021-09-12 23:00:23 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 enum vocabularyList{
-    VOICE_YES = 1,
-    VOICE_NO = 2,
-    VOICE_225 = 225,
-    VOICE_226 = 226,
-    VOICE_227 = 227,
-    VOICE_228 = 228,
-    VOICE_229 = 229
+    VOICE_OPEN = 1,
+    VOICE_CLOSE = 2,
+    VOICE_OPEN_LIGHT = 3,
+    VOICE_CLOSE_LIGHT = 4,
+    VOICE_BRIGHTER = 5,
+    VOICE_DARK = 6,
+    VOICE_SWITCH_COLOR = 7,
+    VOICE_START = 8,
+    VOICE_STOP = 9,
+    VOICE_SWITCH_MODE = 10
 
 }
 
@@ -27,7 +30,7 @@ namespace mASR{
     }
 
     let asrEventId = 3500
-    let lastvoc = vocabularyList.VOICE_YES
+    let lastvoc = vocabularyList.VOICE_OPEN
     //% block="ASR sensor IIC port hear %vocabulary"
     //% vocabulary.fieldEditor="gridpicker" vocabulary.fieldOptions.columns=3
     //% blockId = ASR
@@ -36,21 +39,35 @@ namespace mASR{
         control.inBackground(function () {
             while (true) {
                 const readData = serial.readBuffer(1).toArray(NumberFormat.UInt8BE);
-                if (225==readData[0]) {
-                    lastvoc = vocabularyList.VOICE_225;
+                if (1==readData[0]) {
+                    lastvoc = vocabularyList.VOICE_OPEN;
                     control.raiseEvent(asrEventId, lastvoc);
-                }else if(226==readData[0]){
-                    lastvoc = vocabularyList.VOICE_226;
+                }else if(2==readData[0]){
+                    lastvoc = vocabularyList.VOICE_CLOSE;
                     control.raiseEvent(asrEventId, lastvoc);
-
-                }else if(227==readData[0]){
-                    lastvoc = vocabularyList.VOICE_227;
+                }else if(3==readData[0]){
+                    lastvoc = vocabularyList.VOICE_OPEN_LIGHT;
                     control.raiseEvent(asrEventId, lastvoc);
-                }else if(228==readData[0]){
-                    lastvoc = vocabularyList.VOICE_228;
+                }else if(4==readData[0]){
+                    lastvoc = vocabularyList.VOICE_CLOSE_LIGHT;
                     control.raiseEvent(asrEventId, lastvoc);
-                }else if(229==readData[0]){
-                    lastvoc = vocabularyList.VOICE_229;
+                }else if(5==readData[0]){
+                    lastvoc = vocabularyList.VOICE_BRIGHTER;
+                    control.raiseEvent(asrEventId, lastvoc);
+                } else if (6 == readData[0]) {
+                    lastvoc = vocabularyList.VOICE_DARK;
+                    control.raiseEvent(asrEventId, lastvoc);
+                } else if (7 == readData[0]) {
+                    lastvoc = vocabularyList.VOICE_SWITCH_COLOR;
+                    control.raiseEvent(asrEventId, lastvoc);
+                } else if (8 == readData[0]) {
+                    lastvoc = vocabularyList.VOICE_START;
+                    control.raiseEvent(asrEventId, lastvoc);
+                } else if (9 == readData[0]) {
+                    lastvoc = vocabularyList.VOICE_STOP;
+                    control.raiseEvent(asrEventId, lastvoc);
+                } else if (10 == readData[0]) {
+                    lastvoc = vocabularyList.VOICE_SWITCH_MODE;
                     control.raiseEvent(asrEventId, lastvoc);
                 }
                 basic.pause(100);
@@ -61,24 +78,35 @@ namespace mASR{
 
     //% block="asr_logic %vocabulary"
     //% vocabulary.fieldEditor="gridpicker" vocabulary.fieldOptions.columns=3
-    //% blockId = asr_logic
+    //% blockId = "asr_logic"
     export function asr_logic(vocabulary: vocabularyList): boolean {
-        const readData = serial.readBuffer(1).toArray(NumberFormat.UInt8BE);
-
-    
-        if (225 == readData[0]) {
-            return readData[0] == vocabularyList.VOICE_225;
-        } else if (226 == readData[0]) {
-            return readData[0] == vocabularyList.VOICE_226;
-        } else if (227 == readData[0]) {
-            return readData[0] == vocabularyList.VOICE_227;
-        } else if (228 == readData[0]) {
-            return readData[0] == vocabularyList.VOICE_228;
-        } else if (229 == readData[0]) {
-            return readData[0] == vocabularyList.VOICE_229;
+        const readData2 = serial.readBuffer(1).toArray(NumberFormat.UInt8BE);
+        if (1 == readData2[0]) {
+            return vocabulary == vocabularyList.VOICE_OPEN;
+        } else if (2 == readData2[0]) {
+            return vocabulary == vocabularyList.VOICE_CLOSE;
+        } else if (3 == readData2[0]) {
+            return vocabulary == vocabularyList.VOICE_OPEN_LIGHT;
+        } else if (4 == readData2[0]) {
+            return vocabulary == vocabularyList.VOICE_CLOSE_LIGHT;
+        } else if (5 == readData2[0]) {
+            return vocabulary == vocabularyList.VOICE_BRIGHTER;
+        } else if (6 == readData2[0]) {
+            return vocabulary == vocabularyList.VOICE_DARK;
+        } else if (7 == readData2[0]) {
+            return vocabulary == vocabularyList.VOICE_SWITCH_COLOR;
+        } else if (8 == readData2[0]) {
+            return vocabulary == vocabularyList.VOICE_START;
+        } else if (9 == readData2[0]) {
+            return vocabulary == vocabularyList.VOICE_STOP;
+        } else if (10 == readData2[0]) {
+            return vocabulary == vocabularyList.VOICE_SWITCH_MODE;
         }
         return false
     }
+
+
+   
 
     
 }
